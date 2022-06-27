b9f861efa0057bc22268a034ec330a0bcc1167b8
luca_gouvea <luca.gouvea-ext@solvay.com>
2022-06-27 23:14:46 +0100

    script introduction

--- scripts/create_scratch_org.sh
+++ scripts/create_scratch_org.sh
@@ -1,14 +1,17 @@
 username=${1:-"eburyDemo"}
-scratch_org_config=config/project-org-scratch-def.json
+scratch_org_config=config/project-scratch-def.json
 
 echo 'Creating scratch org...'
 sfdx force:org:create -f $scratch_org_config -d 1 -s -a $username -w 30
 
-echo 'Sleeping for 30 seconds to give the scratch org a chance to fully build....'
-sleep 60
+echo 'Sleeping for 20 seconds to give the scratch org a chance to fully build....'
+sleep 20
 
 echo 'Creating open link for scratch org...'
 sfdx force:org:open -u $username -r
 
 echo 'Deploying metadata using source:deploy...'
 sfdx force:source:deploy -u $username -p force-app -w 30
+
+echo "Inserting User to Queue"
+sfdx force:apex:execute -f scripts/apex/hello.apex -u $username
